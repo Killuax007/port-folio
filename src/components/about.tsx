3a28b8b111fa336fb88c1b8c33bfd6180049c5eb
Killuax007 <pandamanoj872@gmail.com>
2025-05-08 08:29:54 +0530

    Update : About page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,17 +1,9 @@
 "use client";
 
 import { motion } from "framer-motion";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { ShineBorder } from "./magicui/shine-border";
-import { useTheme } from "next-themes";
+import { MagicCard } from "./magicui/magic-card";
+import Image from "next/image";
 export default function About() {
-  const { theme } = useTheme();
   const fadeInUp = {
     hidden: { opacity: 0, y: 20 },
     visible: {
@@ -45,7 +37,7 @@ export default function About() {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, margin: "-100px" }}
-          className="max-w-3xl mx-auto mb-16 text-center"
+          className="max-w-3xl mx-auto mb-10 text-center"
         >
           <motion.h2
             variants={fadeInUp}
@@ -53,6 +45,18 @@ export default function About() {
           >
             About Me
           </motion.h2>
+          <motion.div
+            variants={fadeInUp}
+            className="w-20 h-1 mx-auto mb-6 bg-gradient-to-r from-teal-400 to-emerald-500"
+          />
+          <motion.p variants={fadeInUp} className="text-gray-200">
+            I’m a dedicated developer with a strong focus on building responsive
+            websites and scalable backend systems. I’ve completed a Java
+            internship at <span className="font-semibold">@Tutor Mentor</span> ,
+            where I deepened my backend skills. I don’t claim to be a
+            perfectionist—but I’m driven by the desire to improve constantly and
+            deliver high-quality, reliable solutions that make a real impact.
+          </motion.p>
         </motion.div>
 
         <motion.div
@@ -60,22 +64,101 @@ export default function About() {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, margin: "-100px" }}
-          className="  flex justify-center"
+          className="flex flex-col justify-center w-full max-w-xl mx-auto"
         >
+          <motion.div variants={fadeInUp} className="mb-8">
+            <motion.h2
+              variants={fadeInUp}
+              className="mb-6 text-center text-3xl font-bold md:text-4xl"
+            >
+              Education
+            </motion.h2>
+            <div className="">
+              <MagicCard className="rounded-2xl ">
+                <div className="flex p-2 ">
+                  <Image
+                    width={100}
+                    height={100}
+                    className="rounded-full size-15 object-cover"
+                    alt="college"
+                    src={
+                      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746669009/images/qi7lgrassf5fulyimbwz.jpg"
+                    }
+                  />
+                  <div className="w-full flex justify-between  mx-2 ">
+                    <div className="items-center my-2  md:my-0">
+                      <motion.h2
+                        variants={fadeInUp}
+                        className="text-[14px] md:text-xl"
+                      >
+                        Bachelor of Computer Application
+                      </motion.h2>
+                      <motion.p
+                        variants={fadeInUp}
+                        className="text-gray-400 text-[13px]"
+                      >
+                        Kalam Institute of Technology
+                      </motion.p>
+                    </div>
+                    <div>
+                      <motion.p
+                        variants={fadeInUp}
+                        className="text-white text-[10px] md:text-sm    my-2 md:my-1"
+                      >
+                        2021 - 2024
+                      </motion.p>
+                    </div>
+                  </div>
+                </div>
+              </MagicCard>
+            </div>
+          </motion.div>
           <motion.div variants={fadeInUp}>
-            <Card className="relative overflow-hidden max-w-[350px] w-full text-white h-auto">
-              <CardHeader className="">
-                <CardTitle className="text-xl text-white">
-                  Development
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription className="text-gray-400">
-                  I create scalable, performant applications using modern
-                  frameworks and best practices.
-                </CardDescription>
-              </CardContent>
-            </Card>
+            <motion.h2
+              variants={fadeInUp}
+              className="mb-6 text-center text-3xl font-bold md:text-4xl"
+            >
+              Experience
+            </motion.h2>
+            <div className="">
+              <MagicCard className=" rounded-2xl ">
+                <div className="flex p-2 ">
+                  <Image
+                    width={100}
+                    height={100}
+                    className="rounded-full size-15 object-cover"
+                    alt="college"
+                    src={
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbvFllKcHXrPabeN6zxiK0JmMZEGZRk206bw&s"
+                    }
+                  />
+                  <div className="w-full flex justify-between mx-2 ">
+                    <div className="items-center my-2  md:my-0">
+                      <motion.h2
+                        variants={fadeInUp}
+                        className="text-[14px] md:text-xl"
+                      >
+                        Tutor Mentor
+                      </motion.h2>
+                      <motion.p
+                        variants={fadeInUp}
+                        className="text-gray-400 text-[13px]"
+                      >
+                        Java Developer Intern
+                      </motion.p>
+                    </div>
+                    <div>
+                      <motion.p
+                        variants={fadeInUp}
+                        className="text-white  text-[10px] md:text-sm  font-mono  my-2 md:my-1"
+                      >
+                        Sep 2024 - Present
+                      </motion.p>
+                    </div>
+                  </div>
+                </div>
+              </MagicCard>
+            </div>
           </motion.div>
         </motion.div>
       </div>
