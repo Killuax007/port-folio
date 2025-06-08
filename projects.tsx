fa4a51d4cc43e5ba8019594e58787fa10a33cd6f
Killuax007 <pandamanoj872@gmail.com>
2025-06-08 12:35:47 +0530

    Fix : Separate Project section

---
+++ projects.tsx
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Whisperly",
+    description:
+      "Whisperly is an anonymous social messaging app built with Next.js, enabling users to exchange AI-generated messages securely, fostering curiosity, fun, and open interaction.",
+    image:
+      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746674812/images/ai7cy5o7ie14kb75exm7.png",
+    tags: ["Next.js", "Tailwind CSS", "Mongodb", "Nodemailer", "Gemini"],
+    liveUrl: "https://whissperly.vercel.app/",
+    githubUrl: "https://github.com/Killuax007/whissperly",
+  },
+  {
+    id: 2,
+    title: "Medium-Clone",
+    description:
+      "A Medium-style web app for publishing and reading articles, featuring rich text editing, user authentication, responsive design, and seamless content management for writers.",
+    image:
+      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746723818/images/rdc8pkkwoicslslxjasi.png",
+    tags: ["React", "Tailwind CSS", "Node Js", "PostgreSQL", "Prisma"],
+    liveUrl: "https://medium-clone-sigma-five.vercel.app/",
+    githubUrl: "https://github.com/Killuax007/Medium-clone",
+  },
+  {
+    id: 3,
+    title: "Urban-Bites",
+    description:
+      "Urban Bites delivers fresh, delicious meals fast. Order online, track your food in real-time, and enjoy a seamless dining experience.",
+    image:
+      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746758492/images/bmdpybrradlu412yr82l.png",
+    tags: [
+      "React.js",
+      "Tailwind CSS",
+      "Context API",
+      "Node.js",
+      "Nodemailer",
+      "Mongodb",
+    ],
+    liveUrl: "https://github.com/Killuax007/Urban-Bites",
+    githubUrl: "https://github.com/Killuax007/Urban-Bites",
+  },
+  {
+    id: 4,
+    title: "Employee-Management",
+    description:
+      "A simple Spring Boot web application to manage employee records with CRUD operations, RESTful APIs, and integrated database support for efficient data handling.",
+    image:
+      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746759012/images/fgxwiifzoilctbqzlqsb.png",
+    tags: ["React", "Tailwindcss", "Spring-Boot", "MongoDB"],
+    liveUrl: "#",
+    githubUrl: "https://github.com/Killuax007/Employee-Management-",
+  },
+];
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -9,60 +9,7 @@ import { MagicCard } from "./magicui/magic-card";
 import Link from "next/link";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { ShimmerButton } from "./magicui/shimmer-button";
-
-const projects = [
-  {
-    id: 1,
-    title: "Whisperly",
-    description:
-      "Whisperly is an anonymous social messaging app built with Next.js, enabling users to exchange AI-generated messages securely, fostering curiosity, fun, and open interaction.",
-    image:
-      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746674812/images/ai7cy5o7ie14kb75exm7.png",
-    tags: ["Next.js", "Tailwind CSS", "Mongodb", "Nodemailer", "Gemini"],
-    liveUrl: "https://whissperly.vercel.app/",
-    githubUrl: "https://github.com/Killuax007/whissperly",
-  },
-  {
-    id: 2,
-    title: "Medium-Clone",
-    description:
-      "A Medium-style web app for publishing and reading articles, featuring rich text editing, user authentication, responsive design, and seamless content management for writers.",
-    image:
-      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746723818/images/rdc8pkkwoicslslxjasi.png",
-    tags: ["React", "Tailwind CSS", "Node Js", "PostgreSQL", "Prisma"],
-    liveUrl: "https://medium-clone-sigma-five.vercel.app/",
-    githubUrl: "https://github.com/Killuax007/Medium-clone",
-  },
-  {
-    id: 3,
-    title: "Urban-Bites",
-    description:
-      "Urban Bites delivers fresh, delicious meals fast. Order online, track your food in real-time, and enjoy a seamless dining experience.",
-    image:
-      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746758492/images/bmdpybrradlu412yr82l.png",
-    tags: [
-      "React.js",
-      "Tailwind CSS",
-      "Context API",
-      "Node.js",
-      "Nodemailer",
-      "Mongodb",
-    ],
-    liveUrl: "https://github.com/Killuax007/Urban-Bites",
-    githubUrl: "https://github.com/Killuax007/Urban-Bites",
-  },
-  {
-    id: 4,
-    title: "Employee-Management",
-    description:
-      "A simple Spring Boot web application to manage employee records with CRUD operations, RESTful APIs, and integrated database support for efficient data handling.",
-    image:
-      "https://res.cloudinary.com/dwajmx8y7/image/upload/v1746759012/images/fgxwiifzoilctbqzlqsb.png",
-    tags: ["React", "Tailwindcss", "Spring-Boot", "MongoDB"],
-    liveUrl: "#",
-    githubUrl: "https://github.com/Killuax007/Employee-Management-",
-  },
-];
+import { projects } from "../../projects";
 
 export default function Projects() {
   return (
@@ -91,64 +38,65 @@ export default function Projects() {
         </motion.div>
 
         <div className=" flex flex-col items-center justify-center cursor-pointer   gap-8 ">
-          {projects.map((project) => (
-            <motion.div
-              key={project.id}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-                transition: {
-                  duration: 0.6,
-                  type: "spring",
-                  stiffness: 100,
-                  damping: 10,
-                },
-              }}
-              viewport={{ once: true, margin: "-100px" }}
-              transition={{ duration: 0.6 }}
-              className="overflow-hidden rounded-xl w-full max-w-xl  border border-gray-700"
-            >
-              <MagicCard
-                gradientFrom="#10b981"
-                gradientTo="#FE8FB5"
-                gradientSize={100}
+          {projects &&
+            projects.map((project) => (
+              <motion.div
+                key={project.id}
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.6,
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 10,
+                  },
+                }}
+                viewport={{ once: true, margin: "-100px" }}
+                transition={{ duration: 0.6 }}
+                className="overflow-hidden rounded-xl w-full max-w-xl  border border-gray-700"
               >
-                <div className=" p-4">
-                  <h3 className="mb-2 text-xl font-bold hover:text-teal-400">
-                    {project.title}
-                  </h3>
-                  <p className="mb-4 text-gray-400 text-wrap">
-                    {project.description}
-                  </p>
-                  <div className="flex gap-2 pb-2">
-                    <Link href={project.liveUrl}>
-                      <Badge className="flex font-bold">
-                        <ExternalLink className="" /> Live
-                      </Badge>
-                    </Link>
-                    <Link className="flex" href={project.githubUrl}>
-                      <Badge className="flex font-bold">
-                        {" "}
-                        <FaGithub className="" /> Source
-                      </Badge>
-                    </Link>
+                <MagicCard
+                  gradientFrom="#10b981"
+                  gradientTo="#FE8FB5"
+                  gradientSize={100}
+                >
+                  <div className=" p-4">
+                    <h3 className="mb-2 text-xl font-bold hover:text-teal-400">
+                      {project.title}
+                    </h3>
+                    <p className="mb-4 text-gray-400 text-wrap">
+                      {project.description}
+                    </p>
+                    <div className="flex gap-2 pb-2">
+                      <Link href={project.liveUrl}>
+                        <Badge className="flex font-bold">
+                          <ExternalLink className="" /> Live
+                        </Badge>
+                      </Link>
+                      <Link className="flex" href={project.githubUrl}>
+                        <Badge className="flex font-bold">
+                          {" "}
+                          <FaGithub className="" /> Source
+                        </Badge>
+                      </Link>
+                    </div>
+                    <div className="flex flex-wrap gap-2">
+                      {project.tags.map((tag) => (
+                        <Badge
+                          key={tag}
+                          variant="outline"
+                          className="hover:font-bold hover:bg-white hover:text-black"
+                        >
+                          {tag}
+                        </Badge>
+                      ))}
+                    </div>
                   </div>
-                  <div className="flex flex-wrap gap-2">
-                    {project.tags.map((tag) => (
-                      <Badge
-                        key={tag}
-                        variant="outline"
-                        className="hover:font-bold hover:bg-white hover:text-black"
-                      >
-                        {tag}
-                      </Badge>
-                    ))}
-                  </div>
-                </div>
-              </MagicCard>
-            </motion.div>
-          ))}
+                </MagicCard>
+              </motion.div>
+            ))}
         </div>
 
         <motion.div
